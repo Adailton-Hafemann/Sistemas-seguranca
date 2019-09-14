b8bf13b803da11f28648ed2270ee253529efba44
silbapps <silvinobrigido@gmail.com>
2019-09-14 18:13:27 -0300

    Projeto Spring Security Faltam Ajuste

---
+++ trabalho/trabalho/src/main/java/com/projetoseguranca/trabalho/TrabalhosegurancaApplication.java
@@ -0,0 +1,13 @@
+package com.projetoseguranca.trabalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhosegurancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhosegurancaApplication.class, args);
+	}
+
+}
